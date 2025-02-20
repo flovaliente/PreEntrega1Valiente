bc530f7df94f4cab04915d3a923e738d7274aa94
Florencia Valiente <flovaliente143@gmail.com>
2025-02-19 23:01:49 -0300

    agrego categorias en shop

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,105 +9,127 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 
 import CartWidget from '../cartWidget/CartWidget';
 
 const pages = ['Shop', 'Rebajas', 'Locales'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Navbar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElShop, setAnchorElShop] = React.useState(null);
 
+    
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
     };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
+    //Controla la apertura/cierre de Shop
+    const handleOpenShopMenu = (event) => {
+        setAnchorElShop(event.currentTarget);
     };
 
-    return(
+    const handleCloseShopMenu = () => {
+        setAnchorElShop(null);
+    };
+
+    return (
         <AppBar position="fixed" sx={{ width: '100%', backgroundColor: '#f1f1f1' }}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    href="#app-bar-with-responsive-menu"
-                    sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'black',
-                    textDecoration: 'none',
-                    }}
-                >
-                    VALSAA
-                </Typography>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'black',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        VALSAA
+                    </Typography>
 
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                    <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit"
-                    >
-                    <MenuIcon />
-                    </IconButton>
-                    <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorElNav}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                    open={Boolean(anchorElNav)}
-                    onClose={handleCloseNavMenu}
-                    sx={{ display: { xs: 'block', md: 'none' } }}
-                    >
-                    {pages.map((page) => (
-                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                        <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                        </MenuItem>
-                    ))}
-                    </Menu>
-                </Box>
-                
-                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex',  } }}>
-                    {pages.map((page) => (
-                    <Button
-                        key={page}
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: '#202020', display: 'block' }}
-                    >
-                        {page}
-                    </Button>
-                    ))}
-                </Box>
-                <CartWidget />
+                    {/*Menu hamburguesa en mobile*/}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="menu"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                            keepMounted
+                            transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: 'block', md: 'none' } }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    {/*Menu en compu*/}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) =>
+                            page === 'Shop' ? (
+                                <Box key={page}>
+                                    <Button
+                                        onClick={handleOpenShopMenu}
+                                        sx={{ my: 2, color: '#202020', display: 'block' }}
+                                    >
+                                        {page}
+                                    </Button>
+                                    {/*Menu desplegable de Shop*/}
+                                    <Menu
+                                        id="shop-menu"
+                                        anchorEl={anchorElShop}
+                                        open={Boolean(anchorElShop)}
+                                        onClose={handleCloseShopMenu}
+                                        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                                        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+                                    >
+                                        <MenuItem onClick={handleCloseShopMenu}>Camisas y Blusas</MenuItem>
+                                        <MenuItem onClick={handleCloseShopMenu}>Pantalones y Faldas</MenuItem>
+                                        <MenuItem onClick={handleCloseShopMenu}>Buzos y Sacos</MenuItem>
+                                        <MenuItem onClick={handleCloseShopMenu}>Remeras y Tops</MenuItem>
+                                        <MenuItem onClick={handleCloseShopMenu}>Calzado</MenuItem>
+                                    </Menu>
+                                </Box>
+                            ) : (
+                                <Button
+                                    key={page}
+                                    sx={{ my: 2, color: '#202020', display: 'block' }}
+                                >
+                                    {page}
+                                </Button>
+                            )
+                        )}
+                    </Box>
+                    <CartWidget />
                 </Toolbar>
             </Container>
         </AppBar>
-    )
-}
+    );
+};
 
 export default Navbar;
